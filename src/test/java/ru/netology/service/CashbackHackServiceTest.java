ad24a30454c3010dfc6160ebc5bd9281592d7e67
Artem4ik <artem4iksmirnov1984gmail.com>
2024-04-05 18:57:20 +0300

    CashbackHackService

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testBoundaryExceeds1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testBoundaryLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testBoundary1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+}
