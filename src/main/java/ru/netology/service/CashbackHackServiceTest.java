156be3adf6da500a8f8db3069c8ff004a5b4e183
Artem4ik <artem4iksmirnov1984gmail.com>
2024-04-05 18:36:07 +0300

    CashbackHackService

---
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testBoundaryExceeds1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testBoundaryBelow1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testBoundary1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
